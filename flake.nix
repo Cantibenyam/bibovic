c454a6180e9ca1f5b26cacf6700cc80611bdc399
Imangali Duisebayev <nyamthenyam@gmail.com>
2026-01-22 16:19:52 -0700

    init

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "My NixOS Hyprland Rice";
+
+  inputs = {
+    # NixOS official package source
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    # Replace 'my-hostname' with your actual hostname 
+    # (found inside configuration.nix under networking.hostName)
+    nixosConfigurations.my-hostname = nixpkgs.lib.nixosSystem {
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
